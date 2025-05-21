52a7fa6897a58e8e40c28d9b9d7ee467fa4b07f5
VaDimk12 <komisv4@gmail.com>
2025-05-22 00:53:51 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Animal Tracker API is running!');
+  res.send('Animal Tracker API is runnin!');
 });
 
 const PORT = process.env.PORT || 3000;
